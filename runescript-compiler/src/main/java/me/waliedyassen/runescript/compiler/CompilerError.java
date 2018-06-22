ade59591c15c810cbfbb1f13c7efc6770a5de882
Walied K. Yassen <walied.developer@hotmail.com>
2018-06-22 17:31:29 +0300

    Add in the compiler error classes
    - Adds in the CompilerError class.
    - Adds in the LexicalError class.

---
+++ runescript-compiler/src/main/java/me/waliedyassen/runescript/compiler/CompilerError.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 Walied K. Yassen, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package me.waliedyassen.runescript.compiler;
+
+import me.waliedyassen.runescript.commons.document.Range;
+
+/**
+ * Represents an error that has occurred during any of the compiler phases.
+ * 
+ * @author Walied K. Yassen
+ */
+public class CompilerError extends RuntimeException {
+
+	/**
+	 * The serialisation key of the {@link CompilerError} type.
+	 */
+	private static final long serialVersionUID = 5818510240712800528L;
+
+	/**
+	 * The error source code range.
+	 */
+	private final Range range;
+
+	/**
+	 * Constructs a new {@link CompilerError} type object instance.
+	 * 
+	 * @param range
+	 *                the error source code range.
+	 * @param message
+	 *                a message describing why the error has occurred.
+	 */
+	public CompilerError(Range range, String message) {
+		super(message);
+		this.range = range;
+	}
+
+	/**
+	 * Gets the error source code range.
+	 * 
+	 * @return the source code range.
+	 */
+	public Range getRange() {
+		return range;
+	}
+}
---
+++ runescript-compiler/src/main/java/me/waliedyassen/runescript/compiler/lexer/LexicalError.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 Walied K. Yassen, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package me.waliedyassen.runescript.compiler.lexer;
+
+import me.waliedyassen.runescript.commons.document.Range;
+import me.waliedyassen.runescript.compiler.CompilerError;
+
+/**
+ * Represens any error that has occured during the lexical phase (parsing) of the compilation process.
+ * 
+ * @author Walied K. Yassen
+ */
+public class LexicalError extends CompilerError {
+
+	/**
+	 * The serialisation key of the {@link LexicalError} type.
+	 */
+	private static final long serialVersionUID = -7355707302290328841L;
+
+	/**
+	 * Constructs a new {@link LexicalError} type object instance.
+	 * 
+	 * @param range
+	 *                the error source code range.
+	 * @param message
+	 *                a message describing why the error has occurred.
+	 */
+	public LexicalError(Range range, String message) {
+		super(range, message);
+	}
+}
