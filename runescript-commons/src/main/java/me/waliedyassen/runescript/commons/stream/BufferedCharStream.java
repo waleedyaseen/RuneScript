fea7aa0793ed8918ed4d783c4f6b05a91133078a
Walied K. Yassen <13520048+waliedyassen@users.noreply.github.com>
2019-04-07 17:52:36 +0300

    Fix the CR character rolling back and skipping.

--- runescript-commons/src/main/java/me/waliedyassen/runescript/commons/stream/BufferedCharStream.java
+++ runescript-commons/src/main/java/me/waliedyassen/runescript/commons/stream/BufferedCharStream.java
@@ -15,7 +15,7 @@ import me.waliedyassen.runescript.commons.document.LineColumn;
 /**
  * Represents a buffered character stream, it reads all the data from
  * {@link InputStream} and then caches the data into a {@code char[]} object.
- * 
+ *
  * @author Walied K. Yassen
  */
 public final class BufferedCharStream implements CharStream {
@@ -58,11 +58,9 @@ public final class BufferedCharStream implements CharStream {
 
 	/**
 	 * Constructs a new {@link BufferedCharStream} type object instance.
-	 * 
-	 * @param stream
-	 *               the source code input stream.
-	 * @throws IOException
-	 *                     if anything occurs while reading the data from the
+	 *
+	 * @param stream the source code input stream.
+	 * @throws IOException if anything occurs while reading the data from the
 	 *                     specified {@link InputStream}.
 	 */
 	public BufferedCharStream(InputStream stream) throws IOException {
@@ -71,14 +69,11 @@ public final class BufferedCharStream implements CharStream {
 
 	/**
 	 * Constructs a new {@link BufferedCharStream} type object instance.
-	 * 
-	 * @param stream
-	 *                the source code input stream.
-	 * @param tabSize
-	 *                the tab size, reprsents how many spaces should we increase the
+	 *
+	 * @param stream  the source code input stream.
+	 * @param tabSize the tab size, reprsents how many spaces should we increase the
 	 *                column pointer by after the tab special character.
-	 * @throws IOException
-	 *                     if anything occurs while reading the data from the
+	 * @throws IOException if anything occurs while reading the data from the
 	 *                     specified {@link InputStream}.
 	 */
 	public BufferedCharStream(InputStream stream, int tabSize) throws IOException {
@@ -89,19 +84,19 @@ public final class BufferedCharStream implements CharStream {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#take()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public char take() {
 		if (pos >= buffer.length) {
 			return NULL;
 		}
-		// assuming we are always using CRLF for now.
 		if (peek() == '\r') {
 			pos++;
+		}
+		if (pos >= buffer.length) {
+			return NULL;
 		}
 		char ch = buffer[pos++];
 		if (ch == '\n') {
@@ -115,10 +110,8 @@ public final class BufferedCharStream implements CharStream {
 		return ch;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#peek()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public char peek() {
@@ -128,10 +121,8 @@ public final class BufferedCharStream implements CharStream {
 		return buffer[pos];
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#mark()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public void mark() {
@@ -140,10 +131,8 @@ public final class BufferedCharStream implements CharStream {
 		m_column = column;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#reset()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public void reset() {
@@ -156,10 +145,8 @@ public final class BufferedCharStream implements CharStream {
 		m_pos = m_line = m_column = -1;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#rollback(int)
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public void rollback(int count) {
@@ -168,8 +155,15 @@ public final class BufferedCharStream implements CharStream {
 				pos--;
 			} else {
 				char ch = buffer[pos--];
-				// TODO: line position rolling back
-				if (ch == '\t') {
+				if (ch == '\r') {
+					count++;
+				} else if (ch == '\n') {
+					line--;
+					column = 1;
+					if (ch == '\r') {
+						pos--;
+					}
+				} else if (ch == '\t') {
 					column -= tabSize - (column - 1) % tabSize;
 				} else {
 					column--;
@@ -178,24 +172,19 @@ public final class BufferedCharStream implements CharStream {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#hasRemaining()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public boolean hasRemaining() {
 		return pos < buffer.length;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see me.waliedyassen.runescript.commons.stream.CharStream#position()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public LineColumn position() {
 		return new LineColumn(line, column);
 	}
-
 }
