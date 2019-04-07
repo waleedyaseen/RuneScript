ca2d4c0a25542957705912f3e845e4cb01992137
Walied K. Yassen <13520048+waliedyassen@users.noreply.github.com>
2019-04-07 15:12:49 +0300

    Add binary expressions and operators precedence parsing support.

---
+++ runescript-compiler/src/main/java/me/waliedyassen/runescript/compiler/ast/expr/AstBinaryExpression.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2018 Walied K. Yassen, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package me.waliedyassen.runescript.compiler.ast.expr;
+
+import lombok.Getter;
+import me.waliedyassen.runescript.commons.document.Range;
+import me.waliedyassen.runescript.compiler.util.Operator;
+
+/**
+ * Represents a binary operator expression.
+ *
+ * @author Walied K. Yassen
+ */
+public final class AstBinaryExpression extends AstExpression {
+
+	/**
+	 * The left side expression.
+	 */
+	@Getter
+	private final AstExpression left;
+
+	/**
+	 * The binary operator type.
+	 */
+	@Getter
+	private final Operator operator;
+
+	/**
+	 * The right side expression.
+	 */
+	@Getter
+	private final AstExpression right;
+
+	/**
+	 * Constructs a new {@link AstExpression} type object instance.
+	 *
+	 * @param range    the expression source code range.
+	 * @param left     the left side expression.
+	 * @param operator the binary operator type.
+	 * @param right    the right side expression.
+	 */
+	public AstBinaryExpression(Range range, AstExpression left, Operator operator, AstExpression right) {
+		super(range);
+		this.left = left;
+		this.operator = operator;
+		this.right = right;
+	}
+}
--- runescript-compiler/src/main/java/me/waliedyassen/runescript/compiler/util/Operator.java
+++ runescript-compiler/src/main/java/me/waliedyassen/runescript/compiler/util/Operator.java
@@ -26,32 +26,32 @@ public enum Operator {
 	/**
 	 * The equals operator type.
 	 */
-	EQUAL(0, "=", Kind.EQUALS, Associativity.LEFT),
+	EQUAL(8, "=", Kind.EQUALS, Associativity.LEFT),
 
 	/**
 	 * The not equals operator type.
 	 */
-	NOT_EQUAL(0, "!", Kind.NOT_EQUALS, Associativity.LEFT),
+	NOT_EQUAL(8, "!", Kind.NOT_EQUALS, Associativity.LEFT),
 
 	/**
 	 * The less than operator type.
 	 */
-	LESS_THAN(1, "<", Kind.LESS_THAN, Associativity.NONE),
+	LESS_THAN(9, "<", Kind.LESS_THAN, Associativity.LEFT),
 
 	/**
 	 * The greater than operator type.
 	 */
-	GREATER_THAN(1, ">", Kind.GREATER_THAN, Associativity.NONE),
+	GREATER_THAN(9, ">", Kind.GREATER_THAN, Associativity.LEFT),
 
 	/**
 	 * The less than or equals operator type.
 	 */
-	LESS_THAN_OR_EQUALS(1, "<=", Kind.LESS_THAN_OR_EQUAL, Associativity.NONE),
+	LESS_THAN_OR_EQUALS(9, "<=", Kind.LESS_THAN_OR_EQUAL, Associativity.LEFT),
 
 	/**
 	 * The greater than or equals operator type.
 	 */
-	GREATER_THAN_OR_EQUALS(1, ">=", Kind.GREATER_THAN_OR_EQUAL, Associativity.NONE);
+	GREATER_THAN_OR_EQUALS(9, ">=", Kind.GREATER_THAN_OR_EQUAL, Associativity.LEFT);
 
 	/**
 	 * The operators look-up map.
@@ -85,9 +85,7 @@ public enum Operator {
 	/**
 	 * Looks up for the {@link Operator} constant of the specified {@link Kind}.
 	 *
-	 * @param kind
-	 * 		the token kind to look for it's associated operator.
-	 *
+	 * @param kind the token kind to look for it's associated operator.
 	 * @return the {@link Operator} of that token kind if it is present otherwise <code>null</code>.
 	 */
 	public static Operator lookup(Kind kind) {
@@ -100,11 +98,6 @@ public enum Operator {
 	 * @author Walied K. Yassen
 	 */
 	public enum Associativity {
-
-		/**
-		 * The operator is not associative, and is not allowed to be chained.
-		 */
-		NONE,
 
 		/**
 		 * The operator is left associative.
