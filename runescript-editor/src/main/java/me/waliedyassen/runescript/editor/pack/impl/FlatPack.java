76964d8e47ecde1fc2bf6f21c57d4ba6d5a13cb3
Walied K. Yassen <walied.developer@hotmail.com>
2020-09-13 03:47:36 +0300

    Add support for flatfile packing format.

---
+++ runescript-editor/src/main/java/me/waliedyassen/runescript/editor/pack/impl/FlatPack.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Walied K. Yassen, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package me.waliedyassen.runescript.editor.pack.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import me.waliedyassen.runescript.editor.pack.Pack;
+import me.waliedyassen.runescript.editor.pack.PackFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+
+/**
+ * Represents a {@link Pack} implementation that packs to a directory
+ *
+ * @author Walied K. Yassen
+ */
+@Slf4j
+@RequiredArgsConstructor
+public final class FlatPack implements Pack {
+
+    /**
+     * The path of hte directory we will be packing in.
+     */
+    private final Path path;
+
+    /**
+     * {@inheritDoc}
+     */
+    @SneakyThrows
+    @Override
+    public void pack(PackFile file) {
+        System.out.println("Packing");
+        var path = this.path.resolve(String.format("%d-%s", file.getId(), file.getName()));
+        Files.write(path, file.getData(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+}
---
+++ runescript-editor/src/main/java/me/waliedyassen/runescript/editor/pack/provider/impl/FlatPackProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Walied K. Yassen, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package me.waliedyassen.runescript.editor.pack.provider.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.var;
+import me.waliedyassen.runescript.editor.pack.impl.FlatPack;
+import me.waliedyassen.runescript.editor.pack.provider.PackProvider;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * A {@link PackProvider} implementation that is responsible for providing {@link FlatPack}.
+ *
+ * @author Walied K. Yassen
+ */
+@RequiredArgsConstructor
+public final class FlatPackProvider implements PackProvider {
+
+    /**
+     * The path which leads to the root directory that contains the pack databases.
+     */
+    private final Path path;
+
+    /**
+     * {@inheritDoc}
+     */
+    @SneakyThrows
+    @Override
+    public FlatPack create(String packName) {
+        var directory = path.resolve(packName);
+        if (!Files.exists(directory)) {
+            Files.createDirectories(directory);
+        }
+        if (!Files.isDirectory(directory)) {
+            throw new IllegalArgumentException();
+        }
+        return new FlatPack(directory);
+    }
+}
